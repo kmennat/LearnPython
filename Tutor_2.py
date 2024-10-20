d33dd2823ef4d344150c5fa4c4793ad97bccfc2b
kmennat <khattab.mennat@googlemail.com>
2024-10-20 20:55:14 +0200

    commit diver news

--- Tutor_2.py
+++ Tutor_2.py
@@ -1,21 +1,54 @@
 ############################################
-# https://www.youtube.com/watch?v=mvZHDpCHphk&list=PLDoPjvoNmBAyE_gei5d18qkfIe-Z8mocs&index=1
+# Learn Commad Line
+# https://www.youtube.com/watch?v=JVs2Ywy7wGQ&list=PLDoPjvoNmBAxzNO8ixW83Sf8FnLy_MkUT
 ############################################
-# Programming Language That ca do anything
-# Free and Open Source
-# Interpreted  (Prozesse in runtime) vs Compile
-# Interactive  (Ausgabe im Fenster)
+print(type([1, 2, 3, 4, 5, 6]))  # list => List
+
+print(type((1, 2, 3, 4, 5, 6)))  # tuple => Tuple
+
+print(type({"One": 1, "Two": 2, "Three": 3}))  # dict =>Dictionary
+
+#################################################
+# Escape Sequences Characters
+# \b => Back Space
+# \newline  = > Escape New Line + \
+# \\ => Escape Back Slash
+# \' => Escape Single Quotes
+# \" => Escape Double Quotes
+# \n => Line Feed
+# \r => Carriage Return
+# \t => Horizontal Tab
+# \xhh => Character Hex Value
+#################################################
 
 
-# Clean and Easy    (Wenig semikolon und verschiedene ...)
-# Cross Platform  (für ALLE Betribssystem)
-# Expressive  (einfach zu verstehen)
-# OOP
-# Integrated  (mit anderen App)
-# mit Python sind folgenede Apps entwickelt
-### disqus
-### Instagram
-### Spotify
-### Dropbox
-### Uber
-### Reddit
+# Back space
+print("Hello\bWorld")  # Will Remove o
+
+# Escape New Line + \
+print(
+    "Hallo \
+      I love \
+      Python"
+)
+
+# Escape Back Slash
+print("I love Back Slash \\")
+
+# Escape Single Quotes
+print("I love Back Slash 'Test' ")
+
+# Escape Double Quotes
+print('I love Back Slash "Test" ')
+
+# Line Feed
+print("Hallo Welt\nSecond Line")
+
+# Carriage Return
+print("123456\rAbcd")
+
+# Horizontal Tab
+print("Hallo\tPython")
+
+# \xhh => Character Hex Value
+print("\x4F\x73")
